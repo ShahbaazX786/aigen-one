4c2b7a3d0e24c4bfafecf03625440cdf75383d18
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-04-03 22:26:49 +0530

    feat: add gallery section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Gallery from "@/components/custom/Gallery";
 import Hero from "@/components/custom/Hero";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="min-h-screen bg-background p-3 sm:p-4 lg:p-5">
       {/* <Button>Alhamdulillah</Button> */}
       <Hero />
+      <Gallery />
     </main>
   );
 }
