6cd000cc08db3d3264e1cdce896c46698cc99b81
Shaik Shahbaaz Alam <60142641+ShahbaazX786@users.noreply.github.com>
2026-04-03 20:30:17 +0530

    Initial Setup - Add new icons, examples, gallery assets, and Sentry setup (#1)
    * Added new icons + examples + gallery assets
    
    * feat: setup sentry for error tracking / monitoring

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ba4420aae01f96f4375232fddf881701@o4511155795197952.ingest.de.sentry.io/4511155840811088",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
