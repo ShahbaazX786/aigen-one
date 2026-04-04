81e2819970b02190663f1d4c14596c0ea1e5f242
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-04-04 11:24:12 +0530

    feat: add schema instance for client

---
+++ db/index.ts
@@ -0,0 +1,14 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+import * as schema from '@/db/schema';
+
+
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+    throw new Error('DATABASE_URL is not defined');
+}
+
+const sql = neon(dbUrl);
+export const db = drizzle({ client: sql, schema });
